b18728c684dad31ba055b1c59aff38171253dd2f
Stephen Seo <seo.disparate@gmail.com>
2023-08-24 14:58:00 +0900

    Minor tweak to electricity effect

--- src/electricity_effect.h
+++ src/electricity_effect.h
@@ -9,7 +9,7 @@
 
 constexpr int CYLINDER_SUB_COUNT_MAX = 3;
 constexpr int CYLINDER_SPLIT_COUNT = 3;
-constexpr int CYLINDER_SIDES = 8;
+constexpr int CYLINDER_SIDES = 3;
 constexpr float CYLINDER_MAX_RADIUS = 0.03F;
 constexpr float CYLINDER_EDGE_OFFSET = 0.01F;
 
